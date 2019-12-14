46a1226b74f4de99dbfdf95bc3b2e9e9114088b2
zottwickel <scott.g.mckell@gmail.com>
2019-12-14 14:32:58 -0700

    add authorization

--- src/reviews/reviews-router.js
+++ src/reviews/reviews-router.js
@@ -1,21 +1,24 @@
 const express = require('express')
 const path = require('path')
 const ReviewsService = require('./reviews-service')
+const { requireAuth } = require('../middleware/basic-auth')
 
 const reviewsRouter = express.Router()
 const jsonBodyParser = express.json()
 
 reviewsRouter
   .route('/')
-  .post(jsonBodyParser, (req, res, next) => {
-    const { thing_id, rating, text, user_id } = req.body
-    const newReview = { thing_id, rating, text, user_id }
+  .post(requireAuth, jsonBodyParser, (req, res, next) => {
+    const { thing_id, rating, text } = req.body
+    const newReview = { thing_id, rating, text }
 
     for (const [key, value] of Object.entries(newReview))
       if (value == null)
         return res.status(400).json({
           error: `Missing '${key}' in request body`
         })
+
+    newReview.user_id = req.user.id
 
     ReviewsService.insertReview(
       req.app.get('db'),
--- src/things/things-router.js
+++ src/things/things-router.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const ThingsService = require('./things-service')
+const { requireAuth } = require('../middleware/basic-auth')
 
 const thingsRouter = express.Router()
 
@@ -15,12 +16,14 @@ thingsRouter
 
 thingsRouter
   .route('/:thing_id')
+  .all(requireAuth)
   .all(checkThingExists)
   .get((req, res) => {
     res.json(ThingsService.serializeThing(res.thing))
   })
 
 thingsRouter.route('/:thing_id/reviews/')
+  .all(requireAuth)
   .all(checkThingExists)
   .get((req, res, next) => {
     ThingsService.getReviewsForThing(
